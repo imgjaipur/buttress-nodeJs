467ab1ee26b187a687ac325a1a12ccddfba4c667
devanshugautam <devanshug.img@gmail.com>
2021-12-04 11:06:14 +0530

    edit get all site api's

--- src/api/contruction/siteRoutes.js
+++ src/api/contruction/siteRoutes.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const siteRoutes = express.Router();
+const auth = require('./../../lib/authmiddleware');
 const sitecontroller = require('./sitecontroller.js');
-siteRoutes.get('/getsite/:id?',sitecontroller.getsite);
+siteRoutes.get('/getAllsite', auth, sitecontroller.getsite);
 
 // exports.siteRoutes = siteRoutes;
 module.exports = siteRoutes;
-
-
-
-
